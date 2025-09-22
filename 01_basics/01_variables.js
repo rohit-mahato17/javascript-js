dd6ed3561059043ecde2c7fa663de8d8b778d0ad
Rohit Mahato <rohitmahato88935@gmail.com>
2025-09-22 17:42:00 +0000

    basic

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const account_Id = 144553;
+let account_Email = "rohit@google.com";
+var account_password = "12345";
+account_City= "Odisha";
+
+// account_Id = 789;
+
+account_Email ="Github#gmail.com";
+
+account_password="54321";
+
+// console.log(account_Id);
+// console.log(account_Email);
+// console.log(account_password);
+
+console.table([account_Id, account_Email, account_password, account_City]);
+
+
