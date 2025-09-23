d5b3f89dfe008778424efa85d5bd5ba8d79712e8
Rohit Mahato <rohitmahato88935@gmail.com>
2025-09-23 06:09:00 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,8 +16,40 @@
 //"33zxcvb" => NaN
 //true => 1; false => 0
 
-let isLoggedIn =1;
-let isNumber = String(isLoggedIn)
-console.log(isNumber);
-console.log(typeof isNumber);
+// let isLoggedIn =1;
+// let isNumber = String(isLoggedIn)
+// console.log(isNumber);
+// console.log(typeof isNumber);
 
+//   (Operation) //
+
+let value =3;
+let negvaluee = -value;
+// console.log(negvaluee);
+
+// console.log(2+7);
+// console.log(2-7);
+// console.log(2*7);
+// console.log(2**7);
+// console.log(2/7);
+// console.log(2%7);
+
+let atr1="rocky";
+let atr2=" max";
+
+let str3=atr1+atr2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+
+// console.log( (3+4) * 5 % 3);
+
+// console.log(true + true);
+// console.log(false - false);
+
+let gamecmounter = 100
+gamecmounter++;
+console.log(gamecmounter);
