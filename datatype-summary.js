0bc76d9fb4182d6144b9247eb33ee6f03f35237d
Rohit Mahato <rohitmahato88935@gmail.com>
2025-09-23 06:49:03 +0000

    Summary

---
+++ datatype-summary.js
@@ -0,0 +1,35 @@
+// primitive 
+
+// 7 type: string, Number, Boolearn, null, undefine, symbol , BigInt
+
+const score = 100 ;
+const scorevalue = 100.9;
+
+const isLoggedIn = false;
+const outsidetemp = null;
+let userEmail;
+
+const id= symbol('123');
+const anotherId = sumbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 1267453298476n;
+
+
+// Reference (Non prinitive)
+
+//Array, objects, Funtions 
+
+const heros = ["shaktime", "naagraj", "doga"]
+let Obj = {
+    name: "rohit",
+    age: 22,
+    
+}
+
+function myFuntion(){
+    console.log("hello world");
+}
+
+myFuntion();
