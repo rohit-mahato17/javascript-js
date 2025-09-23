e47375ad67761275f18fbf9e0d832634ed1ac473
Rohit Mahato <rohitmahato88935@gmail.com>
2025-09-23 03:52:07 +0000

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,24 @@
+// "use strict";
+
+// console.log(3+3);
+
+console.log("rohit");
+
+let name = "rohit";
+let age = 34;
+let isLoggedIn = false;
+
+// number => to power 53
+//bigint
+//srting
+//boolean => True/False
+//null => standalone value
+//undegine => null
+//symbol => unique for sumbol
+//object
+
+// console.log(typeof"javasvript");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
