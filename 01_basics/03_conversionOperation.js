12474de15a2d2fc89dd31afdddb702494f36085f
Rohit Mahato <rohitmahato88935@gmail.com>
2025-09-23 05:23:58 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// let age = "17";
+// let age = "17zxcvb";
+// let age = null;
+// let age = undefined;
+
+// console.log(typeof age);        
+// console.log(typeof(age));       
+
+// let valueInNumber = Number(age);
+
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber);        
+
+//notes
+//"33" => 33
+//"33zxcvb" => NaN
+//true => 1; false => 0
+
+let isLoggedIn =1;
+let isNumber = String(isLoggedIn)
+console.log(isNumber);
+console.log(typeof isNumber);
+
