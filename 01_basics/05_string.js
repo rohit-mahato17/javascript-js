49f65a54ea86c5a2a846e0663ba65086eb84d4a0
Rohit Mahato <rohitmahato88935@gmail.com>
2025-09-23 13:20:35 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,12 @@
+
+const name = "hitesh";
+const recever = "rohit" ;
+
+console.log(`I am learning with ${name} javascript and my name is ${recever}`)
+
+const gameName = new String ('rohit');
+
+console.log(gameName[0]);
+
+
+// console.log(gameName.length);
