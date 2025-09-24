3dd3b90ba6da346e1d382c3bb5f7ca1d6ccc6fee
Rohit Mahato <rohitmahato88935@gmail.com>
2025-09-24 18:06:45 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+//array
+
+// const myArr = [0, 1, 2, 3, 4, 5];
+
+// console.log(myArr[0])
+
+// console.log(myArr.length);
+// for(let i=0; i<5; i++){
+//     console.log(myArr.length);
+// }
+
+// let array=["red","green","dark"];
+// array.push("orange","lite red");
+// console.log(array);
+
+// value = array
+
+// for(let i=0; i<array.length; i++){
+//     console.log(array[i]);
+// }
+
+
+// const array = ['a','b','c'];
+
+// const iterator = array.entries();
+
+// console.log(iterator.next().value);
+// console.log(iterator.next().value);
+// console.log(iterator.next().value);
+// console.log(iterator.next().value);
+
+// const a= ['a','b','c'];
+// for(const [index,element]of a.entries()){
+//     console.log(index, element);
+// }
+
+const array=[1,2,3,4,5];
+
+array.push(6,7,8,9);
+array.pop;
+
+console.log(array);
